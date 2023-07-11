b8bf06c5227dcb7a8427c9aacfa088317bfa3569
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-07-11 17:28:52 +0200

    feat: navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,57 @@
-import { UIcon } from '../.nuxt/components';
+
+<script setup>
+const links = [ {
+  label: 'Dashboard',
+  icon: 'i-heroicons-home',
+  to: '/dashboard'
+}, {
+  label: 'Users',
+  icon: 'i-heroicons-user-group',
+  to: '/users'
+}, {
+  label: 'Vaccines',
+  icon: 'i-heroicons-eye-dropper',
+  to: '/vaccines'
+}
+,{
+  label: 'Mothers',
+  icon: 'i-heroicons-user-plus',
+  to: '/mothers'
+},
+{
+  label: 'Children',
+  icon: 'i-heroicons-user',
+  to: '/children'
+},
+{
+  label: 'Appointment',
+  icon: 'i-heroicons-clipboard',
+  to: '/appointments'
+}
+
+
+]
+</script>
+
 <template>
-    <main>
-     <aside>
-     <div>
-        <UIcon name="i-heroicons-bars-3-20-solid" />
+    <main class="h-[100vh] flex">
+     <aside class="px-1 py-2 w-1/5">
+     <div class="flex space-x-4 items-center pb-6">
+        <UIcon class="text-2xl" name="i-heroicons-bars-3-20-solid" />
          <Logo />
      </div>
-     </aside>
      <div>
-        <header></header>
-        <section>
+        <h4 class="text-gray-300 pb-4 px-2">Menu</h4>
+        <UVerticalNavigation :links="links" />
+     </div>
+     </aside>
+     <div class="w-4/5">
+        <header class="py-3">
+            <div class="w-[14rem]">
+            <UInput icon="i-heroicons-magnifying-glass-20-solid" size="md" color="white" :trailing="false" rounded="rounded-full" placeholder="search..."/>
+            </div>
+        </header>
+        <section class="bg-gray-100 w-full h-full">
             <slot />
         </section>
      </div>
