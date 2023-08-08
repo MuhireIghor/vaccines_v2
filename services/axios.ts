768ffd6dbcae7ae74dcb6433fabea387f3b379d6
MuhireIghor <muhireighor123@gmail.com>
2023-08-08 23:39:11 +0200

    [feat]added the user registration function

--- services/axios.ts
+++ services/axios.ts
@@ -1,8 +1,9 @@
 import Instance from 'axios';
+import { User } from 'interfaces/user';
 
 export const axios = Instance.create({
-    baseURL: 'https://vaccines-backend.onrender.com/api/v1/',
-    timeout: 1000,
+    baseURL: 'https://vaccines-backend.onrender.com/api/v1/'
+    // timeout: 1000,/
   });
 
 interface StatusResponse {
@@ -12,4 +13,13 @@ status: string;
 
 export function getStatus(){
     return axios.get<StatusResponse>('status')
+}
+export async function createUser (userPayload:User){
+  try{
+const {data} = await axios.post('/users',userPayload)
+console.log(`user created ${data}`)
+  }
+  catch(err){
+console.log(`error occured ${err}`)
+  }
 }
