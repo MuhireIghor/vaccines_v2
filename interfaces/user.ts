7e05a3722a16385255626700a1042a4e5de5dc30
MuhireIghor <muhireighor123@gmail.com>
2023-08-08 23:31:31 +0200

    [feat]User interface definition

---
+++ interfaces/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    
+        current_location: string,
+        dob: string,
+        email: string,
+        first_name: string,
+        gender: string,
+        password: string,
+        person_id: number,
+        second_name: string,
+        tel: string,
+        username: string
+      }
