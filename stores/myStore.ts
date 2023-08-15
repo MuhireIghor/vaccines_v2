cdce517179d14ebacdff222688a74b276bb6afbf
MuhireIghor <muhireighor123@gmail.com>
2023-08-16 00:02:37 +0200

    [feat]changed the payload structure on the person endpoint by changig the date format

--- stores/myStore.ts
+++ stores/myStore.ts
@@ -1,45 +1,64 @@
+import { log } from "console"
 import { User } from "interfaces/user"
 import { defineStore } from "pinia"
 import { json } from "stream/consumers"
 
-export const useCounterStore = defineStore('counter', () => {
-    const user:any = ref({
-        email: '',
-        password: '',
-        dob: '',
-        first_name: '',
-        second_name: '',
-        tel: '',
-        username: '',
-        gender: '',
-        currrent_location: '',
-        selected: '' as string ,
-        person_id:0
-          })
-    const loginUser = (userData:any)=>{
-        user.value.email = userData.email
-        user.value.password = userData.password
-        user.value.dob = userData.dob
-        user.value.first_name = userData.firstName
-        user.value.second_name = userData.secondName
-        user.value.tel = userData.tel
-        user.value.username = userData.username
-        user.value.gender = userData.gender
-        user.value.currrent_location = userData.currentLocation
-        user.value.selected = userData.selected
-
+export const useUserStore = defineStore('user', () => {
+    const user:any = ref({})
+    const loginUser = (userData:any)=>{ 
+        user.value = {...userData}
+        console.log(user.value)
+  
     }
-
-    const storedUserData = localStorage.getItem('USER')
-    if(storedUserData !==null){
-   const parsedUserData = JSON.parse(storedUserData)
-   if (typeof parsedUserData === 'object' && parsedUserData !== null) {
-    user.value = { ...user.value, ...parsedUserData };
+if(process.client){
+  const storedUserData = localStorage.getItem('USER')
+  if(storedUserData !==null){
+ const parsedUserData = JSON.parse(storedUserData)
+ if (typeof parsedUserData === 'object' && parsedUserData !== null) {
+  user.value = { ...user.value, ...parsedUserData };
+}
   }
-    }
+
+}
     watch(user,(userval)=>localStorage.setItem('USER',JSON.stringify(userval)),{deep:true})
 
   
   
     return { user,loginUser }
   })
+  export const useVaccineStore = defineStore('vaccine',()=> {
+    const vaccine = ref({
+      category: '',
+      created_on: '',
+      expire_date: '',
+      name: '',
+      period: '',
+      purpose: '',
+      updated_on: '',
+    })
+    const createVaccine = (vaxData:any)=>{
+      vaccine.value.category = vaxData.category
+      vaccine.value.created_on = vaxData.created_on
+      vaccine.value.expire_date = vaxData.expire_date
+      vaccine.value.name = vaxData.name
+      vaccine.value.period = vaxData.period
+      vaccine.value.purpose = vaxData.purpose
+      vaccine.value.updated_on = vaxData.updated_on
+    }
+    if(process.client){
+      const storedVaxData = localStorage.getItem('vax')
+      if(storedVaxData !==null){
+     const parsedVaxData = JSON.parse(storedVaxData)
+     if (typeof parsedVaxData === 'object' && parsedVaxData !== null) {
+      vaccine.value = { ...vaccine.value, ...parsedVaxData };
+    }
+      }
+    
+    }
+        watch(vaccine,(vaxval)=>localStorage.setItem('vax',JSON.stringify(vaxval)),{deep:true})
+    
+      
+      
+        return { vaccine,createVaccine }
+  })
+  
