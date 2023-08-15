a6b9ea711c2083d76285c12810bb984c08248358
MuhireIghor <muhireighor123@gmail.com>
2023-08-16 00:01:16 +0200

    [update]Changed the user state and login handler logic

--- services/axios.ts
+++ services/axios.ts
@@ -1,6 +1,10 @@
 import Instance from 'axios';
 import { User } from 'interfaces/user';
+import { useUserStore } from '../stores/myStore';
+import { createPinia } from 'pinia';
+const pinia = createPinia()
 
+const store = useUserStore(pinia);
 export const axios = Instance.create({
     baseURL: 'https://vaccines-backend.onrender.com/api/v1/'
     // timeout: 1000,/
@@ -16,8 +20,9 @@ export function getStatus(){
 }
 export async function createUser (userPayload:User){
   try{
-const {data} = await axios.post('/users',userPayload)
-console.log(`user created ${data}`)
+const {data} = await axios.post('/persons',userPayload)
+store.loginUser(data)
+console.log(`user created`,data)
   }
   catch(err){
 console.log(`error occured ${err}`)
